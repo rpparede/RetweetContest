2c2b264214bebe9a693cacf0f3e393b02b622fb3
rpparede <rpparede@uwaterloo.ca>
2020-12-02 23:17:00 -0800

    Select random user and code cleanup

--- main.go
+++ main.go
@@ -1,78 +1,51 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
+	"math/rand"
 	"os"
-	"strings"
+	"time"
 
-	"golang.org/x/oauth2"
+	"./twitter"
 )
 
 func main() {
+	key, secret, err := getKeys()
+	if err != nil {
+		panic(err)
+	}
+	client, err := twitter.New(key, secret)
+	if err != nil {
+		panic(err)
+	}
+
+	usernames, err := client.Retweeters("1333932197506650112")
+	if err != nil {
+		panic(err)
+	}
+	//tclient, err := tweeterClient(keys.Key, keys.Secret)
+	//usernames, err := retweeters(tclient, "1333932197506650112")
+
+	fmt.Println(usernames)
+	fmt.Println(len(usernames))
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n := r.Intn(len(usernames))
+	fmt.Printf("Winner is: %s", usernames[n])
+}
+
+func getKeys() (key string, secret string, err error) {
 	var keys struct {
 		Key    string `json:"consumer_key"`
 		Secret string `json:"consumer_secret"`
 	}
+
 	f, err := os.Open("keys.json")
 	if err != nil {
-		panic(err)
+		return "", "", err
 	}
 	defer f.Close()
 	dec := json.NewDecoder(f)
 	dec.Decode(&keys)
-	fmt.Printf("%+v\n", keys)
-
-	req, err := http.NewRequest("POST", "https://api.twitter.com/oauth2/token", strings.NewReader("grant_type=client_credentials"))
-	if err != nil {
-		panic(err)
-	}
-
-	req.SetBasicAuth(keys.Key, keys.Secret)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset-UTF-8")
-	var client http.Client
-	res, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	defer res.Body.Close()
-
-	var token oauth2.Token
-	dec = json.NewDecoder(res.Body)
-	err = dec.Decode(&token)
-	if err != nil {
-		panic(err)
-	}
-
-	var conf oauth2.Config
-	tclient := conf.Client(context.Background(), &token)
-	res2, err := tclient.Get("https://api.twitter.com/1.1/statuses/retweets/1333940412709543936.json?count=100")
-	if err != nil {
-		panic(err)
-	}
-	defer res2.Body.Close()
-	//io.Copy(os.Stdout, res2.Body)
-
-	var retweets []Retweet
-	dec = json.NewDecoder(res2.Body)
-	err = dec.Decode(&retweets)
-	if err != nil {
-		panic(err)
-	}
-	var usernames []string
-	for _, retweet := range retweets {
-		usernames = append(usernames, retweet.User.ScreenName)
-	}
-	fmt.Println(usernames)
-	fmt.Println(len(usernames))
-
-}
-
-type Retweet struct {
-	User struct {
-		ScreenName string `json:"screen_name"`
-	} `json:"user"`
+	return keys.Key, keys.Secret, nil
 }
